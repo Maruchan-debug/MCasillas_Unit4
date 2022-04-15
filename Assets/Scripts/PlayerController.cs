f036154546d326a7100eb5bb603a769c9ac24d8c
Maruchan-debug <78717496+Maruchan-debug@users.noreply.github.com>
2022-04-14 22:09:07 -0700

    MCasillas_Unit4LessonB
    MCasillas_Unit4LessonB

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,7 +22,7 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         float magnitude = forwardInput * speed * Time.deltaTime;
-        rbPlayer.AddForce(focalPoint.transform.forward * magnitude, ForceMode.Impulse);
+        rbPlayer.AddForce(focalPoint.transform.forward * magnitude, ForceMode.Force);
 
         Debug.Log("forwardInput" + forwardInput);
         Debug.Log("magnitude" + magnitude);
@@ -33,7 +33,6 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-
             rendererPlayer.material.color = new Color(0.1f + forwardInput, 0.1f, 0.1f + forwardInput);
         }
     }
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject EnemyPrefab;
+    public float spawnRange = 8.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Vector3 genPos = GenerateSpawnPosition();
+        Instantiate(EnemyPrefab, GenerateSpawnPosition(), EnemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    Vector3 GenerateSpawnPosition()
+    {
+        float xPos = Random.Range(-spawnRange, spawnRange);
+        float zPos = Random.Range(-spawnRange, spawnRange);
+        Vector3 spawnPos = new Vector3(xPos, EnemyPrefab.transform.position.y, zPos);
+        return spawnPos;
+    }
+}
